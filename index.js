42c22774955da281de696d37a8eb6042accf0d02
haltinaa <146646260+haltinaa@users.noreply.github.com>
2023-10-31 20:47:30 +0600

    Add files via upload
    njfrzsuvnsdj

---
+++ index.js
@@ -0,0 +1,8 @@
+const playButton = document.getElementById("playButton");
+const audioPlayer = document.getElementById("audioPlayer");
+playButton.addEventListener("click", function () {
+    audioPlayer.play();
+});
+
+
+
